3152de714a26197a3459c8dedb73bba02aed9822
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-18 14:12:33 +0100

    correction atelier2.1 complete

--- src/UseCase/Event.jsx
+++ src/UseCase/Event.jsx
@@ -1,26 +1,36 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-
+import Button from 'react-bootstrap/Button';
 
 export default function Event(props) {
-  
+  const [event, setEvent] = useState(props.event);
+
+  const book=() => {
+    props.fnt();
+    setEvent((e)=> ({
+...e,nbTickets :e.nbTickets-1,
+nbParticipants:e.nbParticipants+1
+
+    }))
+  }
   return (
     <Col  sm={12} md={6} lg={3}>
       <Card>
         <Card.Img
           variant="top"
           style={{ height: 200 }}
-          src={`${props.event.img}`}
+          src={`${event.nbTickets===0 ? "sold_out.png": event.img}`}
         />
         <Card.Body>
           
-            <Card.Title>{props.event.name}</Card.Title>
+            <Card.Title>{event.name}</Card.Title>
           
-          <Card.Text>Price : {props.event.price}</Card.Text>
-          <Card.Text>Number of tickets : {props.event.nbTickets}</Card.Text>
-          <Card.Text>Number of participants : {props.event.nbParticipants}</Card.Text>
-          
+          <Card.Text>Price : {event.price}</Card.Text>
+          <Card.Text>Number of tickets : {event.nbTickets}</Card.Text>
+          <Card.Text>Number of participants : {event.nbParticipants}</Card.Text>
+          <Button variant="primary" onClick={book} disabled={event.nbTickets===0?true:false} >Book an event</Button>
+          <Button variant="danger" onClick={()=>setEvent((e)=>({...e,like:!e.like}))}>{event.like ? "DISLIKE" : "LIKE"}</Button>
          
         </Card.Body>
       </Card>
--- src/UseCase/Events.jsx
+++ src/UseCase/Events.jsx
@@ -2,18 +2,41 @@ import eventsJson from "../data/events.json";
 import React, { useEffect, useState } from "react";
 import Event from "./Event";
 import Row from "react-bootstrap/Row";
-
+import Alert from 'react-bootstrap/Alert';
 
 export default function Events() {
+    const [showAlert, setShowAlert] = useState(false);
+    const [welcomeAlert, setWelcomeAlert] = useState(true);
+    useEffect(() => {
+setTimeout(()=>{
+
+    setWelcomeAlert(false);
+
+}, 3000)
 
 
+    },[])
+
+
+    const showAlertBook =()=>{
+        setShowAlert(true);
+        setTimeout(()=>{
+            setShowAlert(false);
+        }, 2000)
+     }
   return (
     <div>
-     
-      
-      <Row >
+         {welcomeAlert && <Alert  variant="info">
+         Welcome to our App EVENT
+        </Alert>
+       }
+     {showAlert && <Alert  variant="info">
+          You have booked an event !
+        </Alert>
+       }
+     <Row >
         {eventsJson.map((eventItem, index) => (
-          <Event key={index} event={eventItem}  />
+          <Event key={index} event={eventItem}  fnt={showAlertBook}/>
         ))}
       </Row>
     
